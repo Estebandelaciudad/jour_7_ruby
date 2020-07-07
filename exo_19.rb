cdb2a35f7cc16f4f465209e80718c343c7eae9d1
RigateStudio <67631328+RigateStudio@users.noreply.github.com>
2020-07-07 22:36:43 +0200

    Add files via upload
    last commit file 19

--- exo_19.rb
+++ exo_19.rb
@@ -1,17 +1,7 @@
-arr= ["jean.dupont.01@email.fr"]
-50.times do |i|
-arr << "jean.dupont.0#{i+2}@email.fr"
+arr= ["jean.dupont.02@email.fr"]
+25.times do |i|
+arr << "jean.dupont.0#{(i*2)+4}@email.fr"
 end
-if(i.even?)
-puts "jean.dupont.0#{i+2}@email.fr"
-end
+puts arr
 
-#DOESN'T WORK
-
-#arr= ["jean.dupont.01@email.fr"]
-#50.times do |i|
-#if(i.even?)
-#	then
-#arr << "jean.dupont.0#{i+2}@email.fr"
-#end
-#puts arr
+#WORKS
