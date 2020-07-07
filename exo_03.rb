6b81e72318b9a57f8a8e4b6b43ceba3e54b20829
Lucas <lucas.gautier@live.fr>
2020-07-07 21:37:34 +0200

    Lucas - estebandenarval

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+print "Bonjour, Monde ! "
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# La différence principale réside dans le fait que puts revient a la ligne après l'éxécution de sa commande et print ne revient pas à la ligne
